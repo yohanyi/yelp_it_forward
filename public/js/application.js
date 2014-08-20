98bf4db4d11e128b3a29ea3dc57541c46fb570f4
yohanyi <yohanyista@gmail.com>
2014-08-19 18:21:22 -0700

    added initial working google maps

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,38 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+////////////GEOLOCATION////////////////
+navigator.geolocation.getCurrentPosition( function(position) {
+  initialize(position.coords.latitude,position.coords.longitude)
 });
+
+////////////GOOGLE MAPS////////////////
+function initialize(lat,long){
+  var userLocation = new google.maps.LatLng(lat, long);
+  var findBtn = $("#find-me")[0];
+  var zoomBtn = $("#zoom-in")[0];
+
+  var mapOptions = {
+    center: userLocation,
+    zoom: 12
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+
+  google.maps.event.addDomListener(findBtn, 'click', function() {
+    placeMarker(userLocation,map)
+    })
+
+  google.maps.event.addDomListener(zoomBtn, 'click', function() {
+      map.setZoom(18);
+    });
+};
+
+function placeMarker(location,map,markers) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    animation: google.maps.Animation.DROP,
+    title: "Your Current Position"
+  });
+};
+
+
+google.maps.event.addDomListener(window, 'load', initialize)
+
