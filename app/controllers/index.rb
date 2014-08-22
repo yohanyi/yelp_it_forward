003cb7f89c08071d1ef13dfb3528f391a4d6fd9a
yohanyi <yohanyista@gmail.com>
2014-08-22 00:02:53 -0700

    dropped yelp pins with data at midpoint

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,7 +8,6 @@ client = Yelp::Client.new({ consumer_key: "KQlW2qJjSqmX0ZR7nGKS7Q",
                         })
 
 get '/' do 
-  p client
   erb :index
 end
 
@@ -16,13 +15,9 @@ get '/yelp_search' do
   find_input = params[:find_input]
   start_address = params[:from]
   dest_address = params[:to]
-  yelp_locations = []
-
   point = Geocoder::Calculations.geographic_center([start_address,dest_address])
   coordinates = {latitude: point[0], longitude: point[1]}
-  p coordinates
   response = client.search_by_coordinates(coordinates, { term: "#{find_input}",limit: 15, sort: 1})
-  
   response.to_json
 
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,17 +1,14 @@
-////////////GEOLOCATION////////////////
+////////set geolocation
 navigator.geolocation.getCurrentPosition( function(position) {
   initialize(position.coords.latitude,position.coords.longitude)
 });
 var address = [];
 var geocoder;
 var map;
-var result = [];
 
-////////////GOOGLE MAPS////////////////
+//// init maps
 function initialize(lat,long){
   var userLocation = new google.maps.LatLng(lat, long);
-  // var findBtn = $("#find-me")[0];
-  // var zoomBtn = $("#zoom-in")[0];
 
   var mapOptions = {
     center: userLocation,
@@ -26,21 +23,135 @@ var infowindow = new google.maps.InfoWindow({
       maxWidth: 160
     });
 
-/// PLACE MARKER
-// function codeAddress(adrs) {
-//   geocoder.geocode( { 'address': adrs}, function(results, status) {
-//     if (status == google.maps.GeocoderStatus.OK) {
-//       map.setCenter(results[0].geometry.location);
-//       var marker = new google.maps.Marker({
-//           map: map,
-//           position: results[0].geometry.location
-//       });
-//     } else {
-//       alert("Geocode was not successful for the following reason: " + status);
-//     }
-//   });
-// }
 
 
+var calculateRoute = function(from, to){
+  var directionsService = new google.maps.DirectionsService();
+  var directionsRequest = {
+    origin: from,
+    destination: to,
+    travelMode: google.maps.DirectionsTravelMode.WALKING,
+    unitSystem: google.maps.UnitSystem.METRIC
+  };
 
+  directionsService.route(
+    directionsRequest,
+    function(response, status){
+      if (status == google.maps.DirectionsStatus.OK)
+      {
+        new google.maps.DirectionsRenderer({
+          map: map,
+          directions: response
+        });
+      }
+      else
+        $("#error").append("Unable to retrieve your route<br />");
+    }
+  );
+};
+
+var FindLoc = function() {
+  var addressId = this.id.substring(0, this.id.indexOf("-"));
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+      "location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+    },
+    function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK)
+        $("#" + addressId).val(results[0].formatted_address);
+      else
+        $("#error").append("Unable to retrieve your address<br />");
+    });
+  },
+  function(positionError){
+    $("#error").append("Error: " + positionError.message + "<br />");
+  },
+  {
+    enableHighAccuracy: true,
+    timeout: 10 * 1000 // 10 seconds
+  });
+};
+
+
+var getYelp = function(event) {
+  console.log("here")
+  event.preventDefault();
+  $.ajax({
+    url: '/yelp_search',
+    type: 'GET',
+    dataType: 'json',
+    data: $("form").serialize(),
+  })
+  .done(function(result) {
+    console.log("success");
+    MakeMyMapAwesome(result);
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  }); 
+}
+
+function MakeMyMapAwesome(result) {
+  pushAdresses(result);
+  addYelpMarkers(result);
+
+}
+var CalcRoute = function(){
+  calculateRoute($("#from").val(), $("#to").val());
+}
+
+function pushAdresses(result){
+  for(i=0;i<result.businesses.length;i++){
+    var word = result.businesses[i].location.display_address;
+    var newAddress = word[0] + ' ' + word[1] + ', ' + word[2];
+    address.push(newAddress);
+  };
+};
+
+
+function MarkerNinfo(adrs, result) {
+  var info_template = '<div id="content">'+ 
+     '<h1>' +  result.businesses[i].name + '</h1>' +
+     '<p>' + result.businesses[i].display_phone + '</p>' +
+     '<p>' + result.businesses[i].snippet_text + '</p>' +
+     '<a href='+ result.businesses[i].url +'>' + result.businesses[i].url + '</p>' +
+      '</div>';
+
+  geocoder.geocode( { 'address': adrs}, function(adrs, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(adrs[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: adrs[0].geometry.location,
+          animation: google.maps.Animation.DROP
+      });
+    }
+  google.maps.event.addListener(marker, 'click', function() {
+  infowindow.setContent(info_template);
+  infowindow.open(map,marker);
+    });
+  });
+}
+
+
+function addYelpMarkers(result) {
+  for(i=0;i<address.length;i++) {
+    MarkerNinfo(address[i],result)
+  }
+}
+
+var initEvents = function(){
+  $("#calculate-route").on("click", CalcRoute);
+  $("#submit").on("click", getYelp)
+  $("#from-link, #to-link").on("click", FindLoc);
+  google.maps.event.addDomListener(window, 'load', initialize)  
+}
+
+initEvents();
+      
 
